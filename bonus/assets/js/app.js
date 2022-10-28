d634dafd2f1c2e0eaf52caae2d09e684bd785b53
RodrigoFantechi <rodrifante595@libero.it>
2022-10-28 16:09:55 +0200

    finish exercise add bonus

---
+++ bonus/assets/js/app.js
@@ -0,0 +1,55 @@
+/* Descrizione:
+
+Rifare l'esercizio della to do list. Questa volta però ogni todo sarà un oggetto, formato da due proprietà:
+text, una stringa che indica il testo del todo
+done, un booleano (true/false) che indica se il todo è stato fatto oppure no
+MILESTONE 1
+Stampare all'interno di una lista, un item per ogni todo. Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
+MILESTONE 2
+Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, il todo viene rimosso dalla lista.
+MILESTONE 3
+Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
+Bonus:
+1- oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il todo alla lista
+2- cliccando sul testo dell'item, invertire il valore della proprietà done del todo corrispondente (se done era uguale a false, impostare true e viceversa)
+ */
+
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            newTask: {
+                text: null,
+                done: false
+            },
+            error: false,
+            tasks: []
+        }
+    },
+    methods: {
+        addTask() {
+            if (this.newTask.text === null || this.newTask.text === '') {
+                this.error = true;
+            } else {
+                this.tasks.unshift(this.newTask);
+                this.error = false;
+            }
+            this.newTask = {
+                text: null,
+                done: false
+            };
+        },
+        removeTask(taske) {
+            this.tasks.splice(taske, 1)
+        },
+        underline(singleTask) {
+            if (singleTask.done === true) {
+                singleTask.done = false
+            } else {
+                singleTask.done = true
+            }
+        }
+    }
+}).mount('#app')
+
